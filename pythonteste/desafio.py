05e7350042cf755a9e77365964b6a7f86ebf3614
bruhvitoria-tech <brunavitoria07.cruz@gmail.com>
2025-07-29 11:53:58 +0000

    CURSO DE PYTHON 29/07/25

---
+++ pythonteste/desafio.py
@@ -0,0 +1,18 @@
+#Desafio 003
+'''n1=int(input('Digite um valor: '))
+n2=int(input('Digite outro valor: '))
+
+s = n1+n2
+print('A soma entre {} e {} = {}'.format(n1, n2,s))'''
+
+#Desafio 004
+n=input('Digite algo: ')
+print('O tipo primitivo desse valor é: ', type(n))
+print('Só tem espaços?', n.isspace())
+print('É alfabetico?', n.isalpha())
+print('É um número?', n.isnumeric())
+print('É alfanumerico?', n.isalnum())
+print('Está maiuscula?', n.isupper())
+print('Está em minuscula?', n.islower())
+print('Esta capitalizada?', n.istitle())
+
