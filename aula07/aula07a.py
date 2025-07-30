f26f814b6a1e9b41c89f5f14403f32936dc8be38
bruhvitoria-tech <brunavitoria07.cruz@gmail.com>
2025-07-30 11:51:55 +0000

    Estudos 30/07/2025;

---
+++ aula07/aula07a.py
@@ -0,0 +1,33 @@
+'''nome = input('Qual é o seu nome?')
+print('Prazer em conhcer {:2}'.format(nome))'''
+
+n1 = int(input('Um valor: '))
+n2 = int(input('Outro valor: '))
+s= n1+n2
+m = n1 * n2
+d = n1/n2
+di = n1//n2
+e = n1**n2
+
+print('A soma é {}, o produto é {}, divisão é {:.3}'.format(s,m,d), end=', ')
+print('Divisão inteira {} e potência {}'.format(di, e))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
