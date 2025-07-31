eb4c4f189a966205209d7541281df2d250aa2e77
bruhvitoria-tech <brunavitoria07.cruz@gmail.com>
2025-07-31 11:43:31 +0000

    Estudos 31/07/2025.

---
+++ desafios/005.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um numero: '))
+ant = n - 1
+sucs = n +1
+
+print('O valor é {}, seu antecessor é {} e seu sucessor {}'.format(n, ant, sucs))
---
+++ desafios/006.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um numero: '))
+
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+
+print('O dobro do numero é {}'.format(d))
+print('O triplo: {}'.format(t))
+print('Sua raiz quadrada: {}'.format(r))
