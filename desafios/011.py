629c13e7287a9675b148daf20c8101151bd0e066
bruhvitoria-tech <brunavitoria07.cruz@gmail.com>
2025-08-19 01:20:00 +0000

    Estudos 18/08/2025

--- desafios/011.py
+++ desafios/011.py
@@ -2,4 +2,10 @@ altura = float(input('Digite a altura da parede em metros: '))
 largura = float(input('Digite a largura da parede em metros: '))
 
 area = altura*largura
-tinta = area*2
+
+print("Sua parede tem dimensão de {}x{} e sua área é de {}m².".format(largura,altura,area))
+
+tinta = area/2
+
+print("Você precisará de {}L para pintas a sua parede.".format(tinta))
+
---
+++ desafios/012.py
@@ -0,0 +1,4 @@
+preco = float(input("Digite o valor de um produto na loja: "))
+
+novoP = preco - ( preco * 5/100)
+print("Esse é o valor com desconto de 5%: {}". format(novoP))
