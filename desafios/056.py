d993d9261ccaefbf3f782e19ffa0f0bfaa136d32
bruhvitoria-tech <brunavitoria07.cruz@gmail.com>
2025-11-02 20:36:57 +0000

    Estudos

---
+++ desafios/056.py
@@ -0,0 +1,25 @@
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0 
+nomevelho = ''
+totmulher20 = 0
+
+for pessoa in range(1, 5):
+    nome = str(input('\nNome da {}ª pessoa: '.format(pessoa))).strip()
+    idade = int(input('Idade da {}ª pessoa: '.format(pessoa)))
+    sexo = str(input('Sexo da {}ª [M/F]: '.format(pessoa))).strip()
+
+    somaidade+=idade
+    if pessoa == 1 and sexo == 'M':
+        maioridadehomem = idade
+        nomevelhp = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 +=1
+
+mediaidade = somaidade/4
+print('\nA média de idade do grupo é de {} anos'.format(mediaidade))
+print('O homem mais velho tem {} anos e se chama {}'.format(maioridadehomem, nomevelho))
+print('Ao todo são {} mulheres com menos de 20 anos.'.format(totmulher20))
